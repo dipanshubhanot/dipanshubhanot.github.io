90c09fe702578b562e476e13ce45021b12ab3552
Deepanshu <dipanshubhanot@gmail.com>
2020-11-03 20:59:47 +1100

    first commit

---
+++ forms/contact.php
@@ -0,0 +1,76 @@
+<?php
+  
+if($_POST) {
+    $name = "";
+    $email = "";
+    $email_title = "";
+    $concerned_department = "";
+    $visitor_message = "";
+    $message = "<div>";
+      
+    if(isset($_POST['name'])) {
+        $visitor_name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+        $message .= "<div>
+                           <label><b>Visitor Name:</b></label>&nbsp;<span>".$name."</span>
+                        </div>";
+    }
+ 
+    if(isset($_POST['email'])) {
+        $email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['email']);
+        $email = filter_var($email, FILTER_VALIDATE_EMAIL);
+        $message .= "<div>
+                           <label><b>Visitor Email:</b></label>&nbsp;<span>".$email."</span>
+                        </div>";
+    }
+      
+    if(isset($_POST['email_title'])) {
+        $email_title = filter_var($_POST['email_title'], FILTER_SANITIZE_STRING);
+        $message .= "<div>
+                           <label><b>Reason For Contacting Us:</b></label>&nbsp;<span>".$email_title."</span>
+                        </div>";
+    }
+      
+    if(isset($_POST['concerned_department'])) {
+        $concerned_department = filter_var($_POST['concerned_department'], FILTER_SANITIZE_STRING);
+        $message .= "<div>
+                           <label><b>Concerned Department:</b></label>&nbsp;<span>".$concerned_department."</span>
+                        </div>";
+    }
+      
+    if(isset($_POST['visitor_message'])) {
+        $visitor_message = htmlspecialchars($_POST['visitor_message']);
+        $message .= "<div>
+                           <label><b>Visitor Message:</b></label>
+                           <div>".$visitor_message."</div>
+                        </div>";
+    }
+      
+    if($concerned_department == "billing") {
+        $recipient = "billing@domain.com";
+    }
+    else if($concerned_department == "marketing") {
+        $recipient = "marketing@domain.com";
+    }
+    else if($concerned_department == "technical support") {
+        $recipient = "tech.support@domain.com";
+    }
+    else {
+        $recipient = "contact@domain.com";
+    }
+      
+    $message .= "</div>";
+ 
+    $headers  = 'MIME-Version: 1.0' . "\r\n"
+    .'Content-type: text/html; charset=utf-8' . "\r\n"
+    .'From: ' . $email . "\r\n";
+      
+    if(mail($recipient, $email_title, $message, $headers)) {
+        echo "<p>Thank you for contacting us, $visitor_name. You will get a reply within 24 hours.</p>";
+    } else {
+        echo '<p>We are sorry but the email did not go through.</p>';
+    }
+      
+} else {
+    echo '<p>Something went wrong</p>';
+}
+?>
